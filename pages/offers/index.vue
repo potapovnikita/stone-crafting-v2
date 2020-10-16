2223bac578a9df86b51d23af5e76ee617e924c8b
potapov.nikita <potapov.nikita@itmh.ru>
2020-10-16 18:20:56 +0300

    add Buy_old

--- pages/offers/index.vue
+++ pages/offers/index.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
     page-container(reverse="true")
         Header(placeholder="true", reverse="true")
-        Buy
+        Buy_old
         SplitLine(reverse="true")
         Footer(reverse="true")
 </template>
 
 <script>
-    import Buy from '~/components/pagesComponents/Buy.vue'
+    import Buy_old from '~/components/pagesComponents/Buy_old.vue'
     import PageContainer from '~/components/PageContainer.vue'
     import Header from '~/components/Header.vue'
     import Footer from '~/components/Footer.vue'
@@ -17,7 +17,7 @@
     export default {
         components: {
             PageContainer,
-            Buy,
+            Buy_old,
             Header,
             Footer,
             SplitLine,
