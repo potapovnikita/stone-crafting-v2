458fcac4f8e87098edfafb61daede32360100212
Федорин Владимир Александрович <fedorin.vladimir@itmh.ru>
2020-12-29 11:32:00 +0500

    Добавил стили для мобильного

--- components/pagesComponents/VirtualMuseum.vue
+++ components/pagesComponents/VirtualMuseum.vue
@@ -5,21 +5,22 @@
         p.virtual-museum_container__lead-text(v-html="lang === 'ru' ? museum.name : museum.nameEng")
 
         .virtual-museum_container__wrapper
-            ul.virtual-museum_container__levels
-                li(v-for="(num, index) in floors" :key="`dot_${index}`")
-                    .virtual-museum_container__wrapper-dot
-                        p.virtual-museum_container__lead-text {{num}}
-                        .virtual-museum_container__dot(:class="{ 'active': num === activeFloor }")
-                        p {{lang === 'ru' ? 'этаж' : 'floor'}}
-                    
+            .virtual-museum_container__wrapper-controls
+                ul.virtual-museum_container__levels
+                    li(v-for="(num, index) in floors" :key="`dot_${index}`")
+                        .virtual-museum_container__wrapper-dot
+                            p.virtual-museum_container__lead-text.virtual-museum_container__lead-text--label {{num}}
+                            .virtual-museum_container__dot(:class="{ 'active': num === activeFloor }")
+                            p.virtual-museum_container__floor-name {{lang === 'ru' ? 'этаж' : 'floor'}}
+                        
 
-            ul.virtual-museum_container__items-names
-                li(
-                    v-for="(virtual, index) in virtuals"
-                    :key="`name_${index}`"
-                    @click="() => selectVirtual(virtual, index)"
-                )
-                    p.virtual-museum_container__lead-text.virtual-museum_container__lead-text--link(v-html="lang === 'ru' ? virtual.name : virtual.nameEng")
+                ul.virtual-museum_container__items-names
+                    li(
+                        v-for="(virtual, index) in virtuals"
+                        :key="`name_${index}`"
+                        @click="() => selectVirtual(virtual, index)"
+                    )
+                        p.virtual-museum_container__lead-text.virtual-museum_container__lead-text--link(v-html="lang === 'ru' ? virtual.name : virtual.nameEng")
             
             ul.virtual-museum_container__items
                 li(
@@ -100,11 +101,12 @@
 
         &__title
             margin-bottom 21px
+            line-height 140%
 
         &__sub-title
             margin-bottom 27px
             font-size 19px
-            line-height 27px
+            line-height 140%
             letter-spacing 0.3em
             text-transform uppercase
             color goldNew
@@ -189,4 +191,154 @@
 
                 @media only screen and (max-width 400px)
                     height 210px
+
+        @media only screen and (max-width 1280px)
+            padding-top 96px
+
+            &__title
+                margin-bottom 14px
+
+            &__sub-title
+                margin-bottom 24px
+                font-size 18px
+
+            &__lead-text
+                font-size 22px
+
+                &--link
+                    max-width 280px
+                    font-size 20px
+
+            &__wrapper
+                max-width unset
+                width 100%
+                min-height 549px
+                margin 0
+                padding 32px 20px 51px
+
+            &__levels
+                padding-left 78px
+                padding-right 75px
+                background url('~assets/img/virtualMuseum/levels.png') 0 53%  no-repeat
+
+            &__items-names
+                margin-bottom 47px
+
+        @media only screen and (max-width 1000px)
+            padding-top 96px
+
+            &__title
+                font-size 44px
+                margin-bottom 7px
+
+            &__sub-title
+                margin-bottom 20px
+                font-size 16px
+
+            &__lead-text
+                font-size 18px
+
+                &--link
+                    max-width 280px
+                    font-size 18px
+
+            &__wrapper
+                min-height 500px
+                padding 25px 15px 40px
+
+            &__levels
+                padding-left 60px
+                padding-right 60px
+                background url('~assets/img/virtualMuseum/levels.png') 0 51%  no-repeat
+
+            &__items-names
+                max-height 85px
+
+        @media only screen and (max-width 850px)
+            padding-top 90px
+
+            &__title
+                font-size 40px
+                margin-bottom 5px
+
+            &__sub-title
+                margin-bottom 16px
+                font-size 14px
+
+            &__lead-text
+                font-size 16px
+
+                &--link
+                    max-width 190px
+                    font-size 16px
+
+            &__wrapper
+                min-height 480px
+                padding 20px 10px 35px
+
+            &__levels
+                padding-left 55px
+                padding-right 55px
+                background url('~assets/img/virtualMuseum/levels.png') 0 48%  no-repeat
+
+            &__items-names
+                max-height 85px
+                margin-bottom 40px
+
+        @media only screen and (max-width 767px)
+            padding-top 74px
+
+            &__title
+                font-size 28px
+                margin-bottom 19px
+
+            &__sub-title
+                margin-bottom 20px
+                font-size 13px
+
+            &__lead-text
+                max-width 308px
+                margin 0 auto
+                font-size 13px
+
+                &--link
+                    max-width unset
+                    margin 0
+                    font-size 22px
+
+                &--label
+                    max-width unset
+                    margin 0
+                    font-size 26px
+
+            &__floor-name
+                font-size 14px
+
+            &__wrapper
+                min-height 480px
+                padding 33px 10px 25px
+
+            &__wrapper-controls
+                display flex
+                margin-bottom 38px
+
+            &__levels
+                flex-direction column
+                padding 0 10px
+                background transparent
+
+            &__items-names
+                flex-direction row
+                max-width 240px
+                max-height unset
+                margin 0
+                padding-left 32px
+
+                & > li:nth-child(3)
+                    margin-bottom 30px
+
+            &__dot
+                margin-top 0
+                margin-bottom 5px
+
 </style>
