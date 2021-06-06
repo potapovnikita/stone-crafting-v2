74f6d6a7955fbb6790f778200569185c41273cf9
Федорин Владимир Александрович <fedorin.vladimir@itmh.ru>
2021-06-06 22:09:01 +0500

    убрал прозрачность

--- components/ui/Popup.vue
+++ components/ui/Popup.vue
@@ -70,7 +70,7 @@
         bottom 0
         left 0
         right 0
-        background rgba(0, 0, 0, 0.86)
+        background-color blackBackground
         opacity 1
         transition opacity .3s linear
 
